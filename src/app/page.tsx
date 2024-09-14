13819c453f2fb4cabf92357dd7771a096d283d97
Abraham E. Tavarez <abeincwireless@gmail.com>
2024-09-13 23:26:13 -0400

    lading style

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,20 +1,32 @@
 import Link from "next/link";
 
-
 export default function Home() {
   return (
     <main className="min-h-screen bg-gradient-to-r from-cyan-500 to-blue-500">
-     <nav className="flex justify-between p-5 bg-slate-800 text-white">
-      <div>Blog Genie</div>
-      <Link href='#'>Login</Link>
-     </nav>
+      <nav className="flex justify-between items-center p-5 bg-slate-800 text-white">
+        <div className="text-4xl font-bold">BlogGen</div>
+        <Link href="/blog" className="font-bold">Login</Link>
+      </nav>
 
-     <section className="flex justify-center mt-80">
-      <div className="bg-slate-800 p-10 text-white font-bold rounded text-center h-72 flex flex-col justify-center">
-        <h2 className="text-3xl mb-5">Start your free trial!</h2>
-        <Link href='/blogs' className="btn">Sign Up</Link>
-      </div>
-     </section>
+      <section className="mt-20 p-10">
+        <h1 className="text-4xl font-bold mb-10">AI Blog Generator: Automating Content Creation with AI</h1>
+        <p className="text-3xl font-semibold">
+          Discover the future of content creation with my AI Blog Generator.
+          This project automates blog post generation, delivering high-quality,
+          SEO-friendly articles in seconds. Built using Next.js, TypeScript,
+          Tailwind CSS, and powered by OpenAI&apos;s API, this tool showcases my
+          ability to develop innovative solutions for modern challenges.
+        </p>
+      </section>
+
+      <section className="flex justify-center mt-40">
+        <div className="bg-slate-800 p-10 text-white font-bold rounded text-center h-72 flex flex-col justify-center">
+          <h2 className="text-3xl mb-5">Start your free trial today</h2>
+          <Link href="/blog" className="btn">
+            Try it for Free!
+          </Link>
+        </div>
+      </section>
     </main>
   );
 }
